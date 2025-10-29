b1c7fd37ab4df0d6f519e5092fdba7d441cf44fa
nyrmaK <kamrynlmay0808@gmail.com>
2025-10-29 18:22:16 -0400

    Refactor Dog class and add Wolf class

--- Dog.java
+++ Dog.java
@@ -1,10 +1,13 @@
-public class Dog extends Animal {
+public class Wolf extends Animal {
     private String breed;
+    private int Age;
+    private boolean Pups;
 
     // Constructor
-    public Dog(String name, int age, String breed) {
-        super(name, age); // Call the superclass constructor
+    public Wolf(String name, int Age, String breed, boolean Pups) {
+        super(name, Age); // Call the superclass constructor
         this.breed = breed;
+        this.Pups = Pups;
     }
 
     // Getter and Setter for breed
@@ -16,10 +19,27 @@ public class Dog extends Animal {
         this.breed = breed;
     }
 
+    public int getAge() {
+        return Age;
+
+    }
+
+    public void setAge(int Age) {
+        this.Age = Age;
+    }
+
+    public boolean getPups() {
+        return Pups;
+    }
+
+    public void setPups(boolean Pups) {
+        this.Pups = Pups;
+    }
+
     // Implementing the abstract method from Animal
     @Override
     public void makeSound() {
-        System.out.println(getName() + " says: Woof!");
+        System.out.println(getName() + " says: OoooOHhhh!");
     }
 
     // Overriding the displayInfo method to include breed
@@ -27,5 +47,6 @@ public class Dog extends Animal {
     public void displayInfo() {
         super.displayInfo(); // Call the superclass method
         System.out.println("Breed: " + getBreed());
+        System.out.println("Has Pups: " + getPups());
     }
 }
