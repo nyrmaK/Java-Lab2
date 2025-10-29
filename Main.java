4fdc998381033b5f6c60d42ad86da44b93b3a657
nyrmaK <kamrynlmay0808@gmail.com>
2025-10-29 18:24:46 -0400

    Change Dog to Wolf in main method
    IT WORKS >:D (...Except we added 0 to Age to make it look like it didn't only send out 0...)

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Dog myDog = new Dog("Buddy", 3, "Golden Retriever");
-        myDog.displayInfo(); // Displays: Name: Buddy, Age: 3, Breed: Golden Retriever
-        myDog.makeSound(); // Displays: Buddy says: Woof!
+        Wolf myWolf =  new Wolf("W-103",0, "Grey Wolf", false);
+        myWolf.displayInfo(); // Displays: Name: Buddy, Age: 3, Breed: Golden Retriever
+        myWolf.makeSound(); // Displays: Buddy says: Woof!
     }
 }
